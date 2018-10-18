5e85d906f165e23541d28811e1bcbac87e9ce277
Frederic Meeuwissen <frederic.meeuwissen@gmail.com>
2018-10-18 23:20:38 +0200

    Move everything into API class

--- api_test.py
+++ api_test.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-
-# First test script to interface with the ING API
-# Currently, it requests an access token
 import datetime
 import requests
 import OpenSSL
@@ -9,57 +6,78 @@ import base64
 import urllib.parse
 import uuid
 import hashlib
-from printrequest import print_request, print_response
+import pprint
 import settings
+import os
+
+pprinter = pprint.PrettyPrinter()
+pprint = pprinter.pprint
 
 
-client_id = settings.client_id
-oauth_endpoint = 'https://api.ing.com/oauth2/token'
-certfolder = 'cert/'
-tlscert = certfolder + 'tls_public.crt'
-tlskey = certfolder + 'tls.key'
-signcert = certfolder + 'signing.crt'
-signkey = certfolder + 'signing.key'
-signpass = None
+class IngApi:
+    host = 'https://api.ing.com'
+    endpoints = {
+        'oauth': '/oauth2/token',
+        'greetings': '/greetings/single'
+    }
+
+    def __init__(self, certsfolder, clientid):
+        self.certsfolder = certsfolder
+        self.tls_key_file, self.tls_key = self.readcert('tls.key')
+        self.tls_crt_file, self.tls_crt = self.readcert('tls.crt')
+        self.sign_key_file, self.sign_key = self.readcert('signing.key')
+        self.sign_crt_file, self.sign_crt = self.readcert('signing.crt')
+        self.clientid = clientid
+
+    def readcert(self, fn):
+        fn = os.path.join(self.certsfolder, fn)
+        with open(fn) as f:
+            return fn, f.read()
+
+    def calc_signature(self, headers, values):
+        if isinstance(headers, str):
+            headers = headers.split(' ')
+        signing_str = '\n'.join(['{}: {}'.format(h,v) for h,v in zip(headers, values)])
+        pkey = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, self.sign_key)
+        signature = OpenSSL.crypto.sign(pkey, signing_str, 'sha256')
+        return base64.b64encode(signature).decode()
+
+    def gen_reqid(self):
+        return str(uuid.uuid4())
+
+    def request_access_token(self, scopes):
+        if not isinstance(scopes, str):
+            scopes = ' '.join(scopes)
+        body = {'grant_type': 'client_credentials',
+                'scope': scopes}
+        body = urllib.parse.urlencode(body)
+
+        endpoint = self.endpoints['oauth']
+
+        date = self.utctime_now()
+        keyid = self.gen_reqid()
+        digest = 'SHA-256=' + base64.b64encode(hashlib.sha256(body.encode()).digest()).decode()
+        signature_headers = '(request-target) date digest x-ing-reqid'
+        signature = self.calc_signature(signature_headers,
+                                        ('post ' + endpoint, date, digest, keyid))
+
+        headers = {'Authorization': ('Signature keyId="{}",'
+                                     'algorithm="rsa-sha256",'
+                                     'headers="{}",'
+                                     'signature="{}"').format(self.clientid, signature_headers, signature),
+                   'X-ING-ReqID': keyid,
+                   'Date': date,
+                   'Content-Type': 'application/x-www-form-urlencoded',
+                   'Digest': digest}
+        resp = requests.post(self.host + endpoint, headers=headers, data=body, cert=(self.tls_crt_file, self.tls_key_file))
+        resp.raise_for_status()
+        return resp.json()
+
+    @staticmethod
+    def utctime_now():
+        return datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
 
 
-def calc_signature(method, path, date, digest, reqid, signkey, signpass=None):
-    signing_str = """(request-target): {method} {path}
-date: {date}
-digest: {digest}
-x-ing-reqid: {reqid}""".format(**locals())
-    with open(signkey) as signkey_f:
-        signkey_s = signkey_f.read()
-    pkey = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, signkey_s, signpass)
-    signature = OpenSSL.crypto.sign(pkey, signing_str, 'sha256')
-    return base64.b64encode(signature).decode()
-
-
-req_data = {'grant_type': 'client_credentials',
-            'scope': 'greetings:view'}
-req_body = urllib.parse.urlencode(req_data)
-
-req_date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-req_id = str(uuid.uuid4())
-req_hasher = hashlib.sha256()
-req_hasher.update(req_body.encode())
-req_digest = "SHA-256=" + base64.b64encode(req_hasher.digest()).decode()
-req_signature = calc_signature('post', '/oauth2/token', req_date, req_digest, req_id, signkey, signpass)
-
-headers = {'Authorization': 'Signature keyId="{}",'\
-                            'algorithm="rsa-sha256",'\
-                            'headers="(request-target) date digest x-ing-reqid",'\
-                            'signature="{}"'.format(client_id, req_signature),
-           'X-ING-ReqID': req_id,
-           'Date': req_date,
-           'Content-Type': 'application/x-www-form-urlencoded',
-           'Digest': req_digest}
-
-resp = requests.post(oauth_endpoint, headers=headers, data=req_body, cert=(tlscert, tlskey))
-
-print('*'*10 + ' Request ' + '*'*10)
-print_request(resp.request)
-print('*'*10 + ' Response ' + '*'*10)
-print_response(resp)
-
-print('Success!' if 200 <= resp.status_code < 300 else 'Fail!')
+if __name__ == '__main__':
+    ing = IngApi('cert/', settings.client_id)
+    pprint(ing.request_access_token('greetings:view'))
