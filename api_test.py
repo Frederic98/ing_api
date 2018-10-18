43e50a8da562fff30541e25bff4bb2ab5c80cd4e
Frederic Meeuwissen <frederic.meeuwissen@gmail.com>
2018-10-19 00:50:03 +0200

    Add API request function

--- api_test.py
+++ api_test.py
@@ -9,6 +9,7 @@ import hashlib
 import pprint
 import settings
 import os
+from typing import Union
 
 pprinter = pprint.PrettyPrinter()
 pprint = pprinter.pprint
@@ -28,6 +29,8 @@ class IngApi:
         self.sign_key_file, self.sign_key = self.readcert('signing.key')
         self.sign_crt_file, self.sign_crt = self.readcert('signing.crt')
         self.clientid = clientid
+        self.session = requests.Session()
+        self.session.cert = (self.tls_crt_file, self.tls_key_file)
 
     def readcert(self, fn):
         fn = os.path.join(self.certsfolder, fn)
@@ -42,6 +45,11 @@ class IngApi:
         signature = OpenSSL.crypto.sign(pkey, signing_str, 'sha256')
         return base64.b64encode(signature).decode()
 
+    def calc_digest(self, payload: Union[str, bytes]):
+        if isinstance(payload, str):
+            payload = payload.encode()
+        return 'SHA-256=' + base64.b64encode(hashlib.sha256(payload).digest()).decode()
+
     def gen_reqid(self):
         return str(uuid.uuid4())
 
@@ -55,23 +63,43 @@ class IngApi:
         endpoint = self.endpoints['oauth']
 
         date = self.utctime_now()
-        keyid = self.gen_reqid()
-        digest = 'SHA-256=' + base64.b64encode(hashlib.sha256(body.encode()).digest()).decode()
+        reqid = self.gen_reqid()
+        digest = self.calc_digest(body)
         signature_headers = '(request-target) date digest x-ing-reqid'
         signature = self.calc_signature(signature_headers,
-                                        ('post ' + endpoint, date, digest, keyid))
+                                        ('post ' + endpoint, date, digest, reqid))
 
         headers = {'Authorization': ('Signature keyId="{}",'
                                      'algorithm="rsa-sha256",'
                                      'headers="{}",'
                                      'signature="{}"').format(self.clientid, signature_headers, signature),
-                   'X-ING-ReqID': keyid,
+                   'X-ING-ReqID': reqid,
                    'Date': date,
                    'Content-Type': 'application/x-www-form-urlencoded',
                    'Digest': digest}
-        resp = requests.post(self.host + endpoint, headers=headers, data=body, cert=(self.tls_crt_file, self.tls_key_file))
+        resp = self.session.post(self.host + endpoint, headers=headers, data=body)
         resp.raise_for_status()
         return resp.json()
+
+    def request(self, method, endpoint: str, access_token: str, body='', header=None):
+        date = self.utctime_now()
+        digest = self.calc_digest(body)
+        reqid = self.gen_reqid()
+        signature_headers = '(request-target) date digest x-ing-reqid'
+        signature = self.calc_signature(signature_headers,
+                                        (method.lower() + ' ' + endpoint, date, digest, reqid))
+        headers = {'Signature': ('keyId="{}",'
+                                 'algorithm="rsa-sha256",'
+                                 'headers="{}",'
+                                 'signature="{}"').format(self.clientid, signature_headers, signature),
+                   'Authorization': 'Bearer ' + access_token,
+                   'X-ING-ReqID': reqid,
+                   'Date': date,
+                   'Content-Type': 'application/x-www-form-urlencoded',
+                   'Digest': digest}
+        if header is not None:
+            headers.update(header)
+        return self.session.request(method, self.host + endpoint, data=body, headers=headers)
 
     @staticmethod
     def utctime_now():
@@ -80,4 +108,9 @@ class IngApi:
 
 if __name__ == '__main__':
     ing = IngApi('cert/', settings.client_id)
-    pprint(ing.request_access_token('greetings:view'))
+    access_token_d = ing.request_access_token('greetings:view')
+    access_token = access_token_d['access_token']
+    # print(access_token)
+    message = ing.request('GET', ing.endpoints['greetings'], access_token)
+    # pprint(dict(message.request.headers))
+    print(message.json()['message'])
